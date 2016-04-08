d83a160d260cf1d0acdb949024e9066cf60d7a89
Jamie McDowell <jamie_mcd23@hotmail.com>
2016-04-08 01:42:26 +0100

    Adding full screen wallpaper view

---
+++ app/src/main/java/com/wallpaperapp/jamie/wallpaperdownloader/WallpaperPageActivity.java
@@ -0,0 +1,15 @@
+package com.wallpaperapp.jamie.wallpaperdownloader;
+
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by jamie on 08/04/2016.
+ */
+public class WallpaperPageActivity extends SingleFragmentActivity {
+
+    public static Intent newIntent(Context context, String url){
+        Intent i = new Intent(context, WallpaperPageActivity.class);
+        i.setData()
+    }
+}
