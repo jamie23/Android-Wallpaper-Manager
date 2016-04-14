3c4fa73a7b9163de390296026d9fab3071576151
Jamie McDowell <jamie_mcd23@hotmail.com>
2016-04-14 23:55:01 +0100

    Setting up scheduler

---
+++ app/src/main/java/com/wallpaperapp/jamie/wallpaperdownloader/WallpaperScheduler.java
@@ -0,0 +1,15 @@
+package com.wallpaperapp.jamie.wallpaperdownloader;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jamie on 14/04/2016.
+ */
+public class WallpaperScheduler extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/wallpaperapp/jamie/wallpaperdownloader/WallpaperSchedulerFragment.java
@@ -0,0 +1,9 @@
+package com.wallpaperapp.jamie.wallpaperdownloader;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by jamie on 14/04/2016.
+ */
+public class WallpaperSchedulerFragment extends Fragment {
+}
